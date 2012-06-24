02fb381ade6d296bf1ad36f086cfc07759f9fac1
Alex Rozumey <brain-geek@yandex.ua>
2012-06-24 19:35:56 +0300

    Initial commit with empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perftest::Application.config.secret_token = '620a05766a2d937b7266c7647ef368575a6510f2d8f6f259f4f2c6c6ee76cb7ee2ed7e5598ce114460b63d451b774d07569532ff549795ca743129a693c4c075'
